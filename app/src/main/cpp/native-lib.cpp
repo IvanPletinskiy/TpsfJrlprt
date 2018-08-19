af232fd1adf430465d42a0db41e7471d06e07097
Handen <ivanhanden@gmail.com>
2018-08-20 01:32:10 +0300

    Working native c++

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,32 @@
 #include <jni.h>
 #include <string>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
-extern "C" JNIEXPORT jstring
+using namespace std;
+using namespace cv;
 
+extern "C"
+{
+void JNICALL Java_com_handen_roadhelper_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                                           jlong matAddrGray,
+                                                                           jint nbrElem) {
+    Mat &mGr = *(Mat *) matAddrGray;
+    for (int k = 0; k < nbrElem; k++) {
+        int i = rand() % mGr.cols;
+        int j = rand() % mGr.rows;
+        mGr.at<uchar>(j, i) = 255;
+    }
+}
+}
+
+//extern "C" JNIEXPORT jstring
+/*
 JNICALL
-Java_com_handen_roadhelper_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
+Java_com_handen_roadhelper_MainActivity_stringFromJNI(JNIEnv *env, jobject ) {
     std::string hello = "Ky";
     return env->NewStringUTF(hello.c_str());
 }
+*/
